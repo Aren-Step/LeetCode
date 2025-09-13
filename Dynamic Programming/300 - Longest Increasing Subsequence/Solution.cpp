d5a1e7c50d7a985952556803b068308899f70cc6
Aren <aren1.step1.arm@gmail.com>
2025-09-13 17:34:35 +0400

    Initial commit

---
+++ Dynamic Programming/300 - Longest Increasing Subsequence/Solution.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Admin on 9/13/2025.
+//
+
+#include "Solution.h"
+
+int main() {
+
+
+    return 0;
+}
---
+++ Dynamic Programming/300 - Longest Increasing Subsequence/Solution.h
@@ -0,0 +1,20 @@
+//
+// Created by Admin on 9/13/2025.
+//
+
+#ifndef LEETCODE_SOLUTION_H
+#define LEETCODE_SOLUTION_H
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int size = static_cast<int>(nums.size());
+
+    }
+};
+
+
+#endif //LEETCODE_SOLUTION_H
